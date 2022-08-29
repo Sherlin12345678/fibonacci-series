40ce54f6d70fe8471c6090f6308dbe0402ea0799
Sherlin12345678 <112100740+Sherlin12345678@users.noreply.github.com>
2022-08-29 21:59:26 +0530

    fibonacci series .py
    creating a python program for giving first 13 no's of fibonacci series

---
+++ fibonacci series .py
@@ -0,0 +1,29 @@
+def fibo(n):
+    a = 0
+    b = 1
+    if n ==  1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+            print(c)
+
+            
+fibo(13)
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
